ea27548232084762fa245236a40b5990e85c13d8
demilv <gonzalo.cano@bootcamp-upgrade.com>
2024-06-30 01:57:32 +0200

    Preparando Bookings

---
+++ components/Bookings/Bookings.jsx
@@ -0,0 +1,124 @@
+import React, { useState, useEffect } from "react";
+import bookingsData from "../../data/roomData.json"
+import { ButtonNewRoom, ButtonSort, ButtonNextBack, ButtonPage, ButtonUnseen} from "./StyledButtonRoom";
+import { TableColumnFlexMain, TableColumnMain, TableContainIdName, TableFirstRow, TableIdNameContainer, TableImg, TableRoomData, TableRow } from "../styledComponents/StyledTablaRoom";
+
+const Room = () => {
+    const [active, setActive] = useState("All")
+    const [sorting, setSorting] = useState("id")
+    const [page, setPage] = useState(0)
+    const [sortedBookings, setSortedBookings] = useState([])
+    const [bookingsMostrar, setBookingsMostrar] = useState([])
+    const [isDisabledBack, setIsDisabledBack] = useState()
+    const [isDisabledNext, setIsDisabledNext] = useState()
+    const [maxPages, setMaxPages] = useState(Math.floor(bookingsData.length / 10))
+
+    const big1 = "big1"
+    const big2 = "big2"
+    const first = "first"
+
+    const handlePageClick = (index) =>{
+        setPage(index)
+    }
+
+    const handleChangeSort = (type) =>{
+        setSorting(type)
+    }
+
+    useEffect(() => {
+        let filteredBookings = bookingsData;
+
+        if (active === "checkingIn") {
+            filteredBookings = filteredBookings.filter(booking => booking.status === "Check In");
+        } else if (active === "checkingOut") {
+            filteredBookings = filteredBookings.filter(booking => booking.status === "Check Out");
+        } else if (active === "inProgress") {
+            filteredBookings = filteredBookings.filter(booking => booking.status === "In Progress");
+        }
+
+        filteredBookings.sort((a, b) => {
+            if (a[sorting] < b[sorting]) return -1;
+            if (a[sorting] > b[sorting]) return 1;
+            return 0;
+        });
+
+        setSortedBookings(filteredBookings);
+        setPage(0);
+
+        setMaxPages(Math.floor(filteredBookings.length / 10));
+    }, [sorting, active]);
+
+      useEffect(() => {        
+        const registros = page*10;
+        setBookingsMostrar (sortedBookings.slice(registros, registros+10))
+      }, [page, sortedBookings, sorting, active]);
+
+      useEffect(() =>{
+        if (page === 0){
+            setIsDisabledBack(true)
+            setIsDisabledNext(false)
+        }else if(page === maxPages){
+            setIsDisabledBack(false)
+            setIsDisabledNext(true)
+        }else{
+            setIsDisabledBack(false)
+            setIsDisabledNext(false)
+        }
+      }, [page])
+
+
+
+    return (
+        <>      
+            <TableFirstRow>
+                <TableRow first={first}>
+                    <ButtonSort active={active === "All"} onClick={() => setActive("All")}>All Bookings</ButtonSort>
+                    <ButtonSort active={active === "checkingIn"} onClick={() => setActive("checkingIn")}>Checking In</ButtonSort>
+                    <ButtonSort active={active === "checkingOut"} onClick={() => setActive("checkingOut")}>Checking Out</ButtonSort>
+                    <ButtonSort active={active === "inProgress"} onClick={() => setActive("inProgress")}>In Progress</ButtonSort>
+                    {/*Aqui pondria un buscador de booking*/}
+                </TableRow>
+                <ButtonNewRoom>+ New Room</ButtonNewRoom>
+            </TableFirstRow>
+            <TableRoomData>
+                <TableRow>
+                    <TableColumnMain big={big1}><ButtonUnseen onClick={()=> handleChangeSort("guest")}>Guest</ButtonUnseen></TableColumnMain>
+                    <TableColumnMain big={big1}><ButtonUnseen onClick={()=> handleChangeSort("order_date")}>Order Date</ButtonUnseen></TableColumnMain>
+                    <TableColumnMain><ButtonUnseen onClick={()=> handleChangeSort("checkIn_date")}>Check in</ButtonUnseen></TableColumnMain>
+                    <TableColumnMain><ButtonUnseen onClick={()=> handleChangeSort("checkOut_date")}>Check out</ButtonUnseen></TableColumnMain>
+                    <TableColumnMain>Special Request</TableColumnMain>
+                    <TableColumnMain>Room Type</TableColumnMain>
+                    <TableColumnMain>Status</TableColumnMain>
+                </TableRow>
+                {bookingsMostrar.map(room => (
+                    <TableRow key={room.id}>
+                        <TableColumnFlexMain>
+                            <TableImg src={room.fotoLink} alt={`Room ${room.number}`}/>
+                            <TableContainIdName>
+                                <TableIdNameContainer>
+                                    #{room.id}
+                                </TableIdNameContainer>
+                                <TableIdNameContainer>
+                                    {room.number}
+                                </TableIdNameContainer>
+                            </TableContainIdName>
+                        </TableColumnFlexMain>
+                        <TableColumnMain>{room.bedType}</TableColumnMain>
+                        <TableColumnMain>{room.floor}</TableColumnMain>
+                        <TableColumnMain big={big2}>{room.amenities}</TableColumnMain>
+                        <TableColumnMain>{room.price}/night</TableColumnMain>
+                        <TableColumnMain>{room.offer}/night</TableColumnMain>
+                        <TableColumnMain>{room.status ? 'Available' : 'Occupied'}</TableColumnMain>
+                    </TableRow>
+                ))}         
+            </TableRoomData>  
+            <ButtonNextBack first={first}onClick={() =>setPage(page-1)} disabled={isDisabledBack}>Back</ButtonNextBack>
+            {Array.from({ length: maxPages + 1 }, (_, index) => (
+                    <ButtonPage key={index} active={index === page}  onClick={() => handlePageClick(index)}>{index + 1}</ButtonPage>
+                ))}
+            <ButtonNextBack onClick={() =>setPage(page+1)} disabled={isDisabledNext}>Next</ButtonNextBack>         
+        </>
+    )
+}
+
+export default Room
---
+++ styledComponents/StyledButtonRoom.jsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const ButtonSort = styled.button`
+  margin-top: 3em;
+  padding: 0 2em 0 2em;
+  width:10em;
+  border: none;
+  border-bottom: 1px solid ${props => props.active ? 'green' : 'grey'};
+  color: ${props => props.active ? 'green' : 'grey'};
+  background-color:transparent;
+`;
+
+export const ButtonNewRoom = styled.button`
+    border-radius:0.4em;
+    background-color: rgb(6, 74, 4);  
+    color: white;
+    margin-top:4em;
+    max-height: 2em;
+    padding: 0.5em 3em 1em 3em;
+    margin-right:5%;
+`
+
+export const ButtonNextBack = styled.button`
+    margin-left: ${props => props.first ? '60%' : ''};
+    border-radius:0.4em;
+    color: rgb(6, 74, 4);
+    border: 1px solid  rgb(6, 74, 4);
+    width:5em;
+    padding: 1em;
+    margin-right:0.5em
+`
+
+export const ButtonPage = styled.button`
+    border-radius:0.4em;
+    min-width: 4.5em;
+    margin-bottom:1em;
+    margin-right:0.5em;
+    padding: 1em 2em;
+    border:none;
+    font-color: ${props => props.active ? 'white' : 'black'};
+    background-color: ${props => props.active ? 'green' : 'transparent'};
+    text-align:center;
+`
+
+export const ButtonUnseen = styled.button`
+    border:none;
+    background-color:transparent;
+    color:black;
+    font-weight:700
+`
+
+
+/*export const SelectTime = styled.select`
+    background-color:white;
+    border-radius:0.4em;
+    border-color: rgb(6, 74, 4);
+    margin-left:2em;
+    color: rgb(6, 74, 4);
+    padding: 1em 5em 1em 2em;
+
+`*/
